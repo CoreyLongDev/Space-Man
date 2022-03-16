86d7edfd82cc77785b05275272cdad59a723e254
PatriotxZero <Coreylong90@icloud.com>
2022-03-15 21:48:06 -0400

    usr input and challenge word inputs matched and now checked if correct. JS, HTML, CSS cleaned up from old comments. looking good. project about 70% complete

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 ////////////////////////
 //pregame and globals//
 //////////////////////
+alert(`Welcome to SpaceMan.
 
-// firstHP.style.display = 'none'
-// secondHP.style.display = 'none'
-// thirdHP.style.display = 'none'
-// letter.style.display = 'none'
+Press the start button to begin.
+Upon starting the game you'll be given a hidden word.
+Guess the letters to reveal the hidden word.
+
+CAREFUL...
+too many guesses can lead your astronaut astray..
+`);
+
+//globals
 let word = "";
+let health = 3;
 
 ////////////////////////////
-//start and reset buttons// -- WORKING, ADDITIONS NEEDED
+//start and reset buttons//
 //////////////////////////
 
+//start game
 function startGame() {
   event.preventDefault();
-  //WHY THE HELL IS THIS DISREGARDING THE CSS FILES INSTRUCTIONS
-//   firstHP.style.display = 'block'
-//   secondHP.style.display = 'block'
-//   thirdHP.style.display = 'block'
-//   letter.style.display = 'block'
   getWord();
+  startButton.style.display = "none";
+  answerHeader.innerText = "Guess a Letter at a time!";
 }
+//reset game
 function resetGame() {
   window.location.reload();
 }
 
 ///////////////////////
-//Word Generator API//  -- WORKING
+//Word Generator API//
 /////////////////////
 
 function getWord(event) {
@@ -36,31 +42,33 @@ function getWord(event) {
     })
 
     .then((res) => {
-      //push fetch to value
-      word = res.pop();
-    //   console.log(word.split(""));
-      word.split("").forEach((char) => letterInput(char));
+      word = res.pop().toUpperCase();
+      console.log(`The Answer Is ${word}`);
+
+      for (let i = 0; i < word.length; i++) {
+        const div = document.createElement("div");
+        div.setAttribute("name", word[i]);
+        div.classList.add("divLetter");
+        // console.log(div.getAttribute("name"));
+        document.querySelector("#letterBoard").append(div);
+      }
     });
-}
-
-/////////////////
-//letter Board// -- WORKING
-////////////////
-
-function letterInput(letter) {
-  let newInput = document.createElement("input");
-  //   type="text" id="letter" maxlength="1"
-  newInput.type = "text";
-  newInput.value = letter;
-  newInput.maxLength = 1;
-  newInput.className = 'letter'
-  const form = document.querySelector("form");
-  form.appendChild(newInput);
 }
 /////////////////
 //answer check//
 ///////////////
+
 function checkAnswer() {
- let answer = document.querySelector('answer')
- console.log(answer)
-}
+  let answer = document.getElementById("answer").value.toUpperCase();
+  let blocks2Array = document.querySelectorAll(".divLetter");
+  let newBlocks = Array.from(blocks2Array).map((x) => x.getAttribute("name"));
+
+  for (i = 0; i < newBlocks.length; i++) {
+    if (answer == newBlocks[i]) {
+      console.log(`${answer} is correct. its located at ${i}`);
+      blocks2Array.innerText += newBlocks[i];
+      blocks2Array[i].style.border = `5px solid green`;
+      blocks2Array[i].style.color = `green`;
+    }
+  }
+} //end of function
